cf49d7efd322a8db0cc914838d5648d4a2706458
Dhruv0210 <153885784+Dhruv0210@users.noreply.github.com>
2023-12-15 10:47:40 +0530

    Add files via upload

---
+++ area.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+ float len,area;
+ printf("enter length of sqare:");
+ scanf("%f",& len);
+ area = len*len;
+ printf("\n area 
+ .")
+ return 0;
+}
